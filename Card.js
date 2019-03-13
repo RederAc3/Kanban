8677af76559777d8fb7f81a4b90b4db4d5006d3f
Reder <40085254+RederAc3@users.noreply.github.com>
2019-03-13 23:43:02 +0100

    Update Card.js

--- Card.js
+++ Card.js
@@ -1,7 +1,7 @@
 // KLASA KANBAN CARD
 function Card(id, name) {
     var self = this;
-x
+    
     this.id = id;
     this.name = name || 'No name given';
     this.element = generateTemplate('card-template', { description: this.name }, 'li');
